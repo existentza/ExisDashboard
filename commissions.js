fc9084af98e4b14a70983ca410aa6e92ce6320bc
existentzwastaken <nonesistod.iscord@gmail.com>
2025-04-10 16:57:39 +0200

    Update Intermission in #78: 80%→70%, status 1→1

--- commissions.js
+++ commissions.js
@@ -2,29 +2,77 @@ const commissionsData = [
     {
         id: "#EI",
         tasks: [
-            { name: "Weapon System", status: 3, percentage: 80, date: "2025/??/??", description: "Waiting for orders..." },
-            { name: "Spawn Menu", status: 3, percentage: 10, date: "2025/??/??", description: "Waiting for your orders." }
-        ]
+            {
+                name: "Weapon System",
+                status: 3,
+                percentage: 80,
+                date: "2025/??/??",
+                description: "Waiting for orders...",
+            },
+            {
+                name: "Spawn Menu",
+                status: 2,
+                percentage: 90,
+                date: "2025/??/??",
+                description: "\"test\"",
+            },
+        ],
     },
     {
         id: "#78",
         tasks: [
-            { name: "Intermission", status: 1, percentage: 80, date: "2025/04/09", description: "Currently working on it..." }
-        ]
+            {
+                name: "Intermission",
+                status: 1,
+                percentage: 70,
+                date: "2025/04/09",
+                description: "Currently working on it...",
+            },
+        ],
     },
     {
         id: "#79",
         tasks: [
-            { name: "Bar Tender System", status: 3, percentage: 0, date: "2025/??/??", description: "" },
-            { name: "Role Manager System", status: 3, percentage: 0, date: "2025/??/??", description: "" },
-            { name: "Team Doors System", status: 3, percentage: 0, date: "2025/??/??", description: "" },
-            { name: "Game Pass Doors", status: 3, percentage: 0, date: "2025/??/??", description: "" }
-        ]
+            {
+                name: "Bar Tender System",
+                status: 3,
+                percentage: 0,
+                date: "2025/??/??",
+                description: "",
+            },
+            {
+                name: "Role Manager System",
+                status: 3,
+                percentage: 0,
+                date: "2025/??/??",
+                description: "",
+            },
+            {
+                name: "Team Doors System",
+                status: 3,
+                percentage: 0,
+                date: "2025/??/??",
+                description: "",
+            },
+            {
+                name: "Game Pass Doors",
+                status: 3,
+                percentage: 0,
+                date: "2025/??/??",
+                description: "",
+            },
+        ],
     },
     {
         id: "#81",
         tasks: [
-            { name: "Drinks System", status: 0, percentage: 0, date: "2025/04/10", description: "" }
-        ]
-    }
+            {
+                name: "Drinks System",
+                status: 0,
+                percentage: 0,
+                date: "2025/04/10",
+                description: "",
+            },
+        ],
+    },
 ];
