5af8b06c3e9eafc9c2d9bb448a32dd19330f1d82
existentzwastaken <nonesistod.iscord@gmail.com>
2025-07-12 17:12:14 +0200

    Add new commission E1 with 4 tasks

--- commissions.js
+++ commissions.js
@@ -1,1 +1,35 @@
-const commissionsData = [];
+const commissionsData = [
+    {
+        id: "E1",
+        tasks: [
+            {
+                name: "Add new maps",
+                percentage: 0,
+                status: 0,
+                description: "",
+                date: "",
+            },
+            {
+                name: "Movement System",
+                percentage: 0,
+                status: 0,
+                description: "",
+                date: "",
+            },
+            {
+                name: "Spectate System",
+                percentage: 0,
+                status: 0,
+                description: "",
+                date: "",
+            },
+            {
+                name: "Check player req",
+                percentage: 0,
+                status: 0,
+                description: "",
+                date: "",
+            },
+        ],
+    },
+];
