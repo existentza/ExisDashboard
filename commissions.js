d824de437632d997d7ecdb0cf1fb95b660f38271
existentzwastaken <nonesistod.iscord@gmail.com>
2025-08-13 23:24:40 +0200

    Snapshot TEST (1 tasks)

--- commissions.js
+++ commissions.js
@@ -1,218 +1,230 @@
 const commissionsData = [
     {
-        id: "E1",
-        tasks: [
+        "id": "E1",
+        "tasks": [
             {
-                name: "Add new maps",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Add new maps",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Movement System",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Movement System",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Check player req",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Check player req",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Knife System",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Knife System",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Waiting Payment",
-                percentage: 100,
-                status: 4,
-                description: "",
-                date: "",
-            },
-        ],
+                "name": "Waiting Payment",
+                "percentage": 100,
+                "status": 4,
+                "description": "",
+                "date": ""
+            }
+        ]
     },
     {
-        id: "E2",
-        tasks: [
+        "id": "E2",
+        "tasks": [
             {
-                name: "Gamepass Shop",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Gamepass Shop",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Emotes Menu",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Emotes Menu",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Tools Tab",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Tools Tab",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Ragdoll Command",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Ragdoll Command",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Remove Sword Fix",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Remove Sword Fix",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "WAITING FOR PAYMENT",
-                percentage: 100,
-                status: 4,
-                description: "",
-                date: "",
-            },
-        ],
+                "name": "WAITING FOR PAYMENT",
+                "percentage": 100,
+                "status": 4,
+                "description": "",
+                "date": ""
+            }
+        ]
     },
     {
-        id: "E5",
-        tasks: [
+        "id": "E5",
+        "tasks": [
             {
-                name: "Name-Tag System",
-                percentage: 100,
-                status: 2,
-                description: "",
-                date: "",
+                "name": "Name-Tag System",
+                "percentage": 100,
+                "status": 2,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Level System",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Level System",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Shop System ",
-                percentage: 30,
-                status: 1,
-                description: "Currently working on UIs",
-                date: "",
+                "name": "Shop System ",
+                "percentage": 30,
+                "status": 1,
+                "description": "Currently working on UIs",
+                "date": ""
             },
             {
-                name: "Gamepasses",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Gamepasses",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Info UI",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Info UI",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Settings UI",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Settings UI",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Music UI / Leaderstats / Whitelist Stuff",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Music UI / Leaderstats / Whitelist Stuff",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Chat Colour / Tag Colour",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
-            },
-        ],
+                "name": "Chat Colour / Tag Colour",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
+            }
+        ]
     },
     {
-        id: "L1",
-        tasks: [
+        "id": "L1",
+        "tasks": [
             {
-                name: "Data Saving & Offline",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Data Saving & Offline",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Machine Placement",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Machine Placement",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Snack Production & Rarity",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Snack Production & Rarity",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Random Events",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Random Events",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Candy Shop Selling",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Candy Shop Selling",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Trading System",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Trading System",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Staff/Worker Automation",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Staff/Worker Automation",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Inventory Management",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
+                "name": "Inventory Management",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
             },
             {
-                name: "Anti Exploit",
-                percentage: 0,
-                status: 0,
-                description: "",
-                date: "",
-            },
-        ],
+                "name": "Anti Exploit",
+                "percentage": 0,
+                "status": 0,
+                "description": "",
+                "date": ""
+            }
+        ]
     },
+    {
+        "id": "TEST",
+        "tasks": [
+            {
+                "name": "Test Task",
+                "percentage": 50,
+                "status": 3,
+                "description": "This is a test, ignore.",
+                "date": "2025/08/20"
+            }
+        ]
+    }
 ];
