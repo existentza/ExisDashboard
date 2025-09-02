604989e74511d036a62dd76ba4cae2ca2e5dab35
existentzwastaken <nonesistod.iscord@gmail.com>
2025-09-02 15:35:57 +0200

    Snapshot E1

--- commissions.js
+++ commissions.js
@@ -1,45 +1,5 @@
 const commissionsData = [
     {
-        "id": "E1",
-        "tasks": [
-            {
-                "name": "Add new maps",
-                "percentage": 100,
-                "status": 2,
-                "description": "",
-                "date": ""
-            },
-            {
-                "name": "Movement System",
-                "percentage": 100,
-                "status": 2,
-                "description": "",
-                "date": ""
-            },
-            {
-                "name": "Check player req",
-                "percentage": 100,
-                "status": 2,
-                "description": "",
-                "date": ""
-            },
-            {
-                "name": "Knife System",
-                "percentage": 100,
-                "status": 2,
-                "description": "",
-                "date": ""
-            },
-            {
-                "name": "Waiting Payment",
-                "percentage": 100,
-                "status": 4,
-                "description": "",
-                "date": ""
-            }
-        ]
-    },
-    {
         "id": "A3",
         "tasks": [
             {
