f517a46e780f212111978ec12c51dadda1b4aff5
existentzwastaken <nonesistod.iscord@gmail.com>
2025-10-25 02:02:54 +0200

    Snapshot A3 (9 tasks)

--- commissions.js
+++ commissions.js
@@ -14,56 +14,56 @@ const commissionsData = [
                 "percentage": 0,
                 "status": 0,
                 "description": "",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Cosmetics",
                 "percentage": 0,
                 "status": 3,
                 "description": "Waiting for assets...",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Shop System",
                 "percentage": 70,
                 "status": 1,
                 "description": "Waiting for other assets",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Currency System",
                 "percentage": 100,
                 "status": 2,
                 "description": "",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Datasaving",
                 "percentage": 100,
                 "status": 2,
                 "description": "",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Leaderboards",
                 "percentage": 90,
                 "status": 3,
                 "description": "Waiting for info",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Quests",
                 "percentage": 0,
                 "status": 3,
                 "description": "Waiting more infos",
-                "date": ""
+                "date": "2025/10/31"
             },
             {
                 "name": "Inventory System",
                 "percentage": 100,
                 "status": 2,
                 "description": "Done!",
-                "date": ""
+                "date": "2025/10/31"
             }
         ]
     },
