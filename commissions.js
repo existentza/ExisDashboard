506fedf34265811113ea3c180f927820713255ec
existentzwastaken <nonesistod.iscord@gmail.com>
2025-10-30 16:06:42 +0100

    Update Round System in A3: 60%→80%, ⚙️ In progress→⚙️ In progress

--- commissions.js
+++ commissions.js
@@ -4,7 +4,7 @@ const commissionsData = [
         "tasks": [
             {
                 "name": "Round System",
-                "percentage": 60,
+                "percentage": 80,
                 "status": 1,
                 "description": "",
                 "date": "2025/10/31"
